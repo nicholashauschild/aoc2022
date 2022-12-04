94348f9941469b6d0145bd067c39bf2f8f3165b7
Nick Hauschild <nhauschild@phdata.io>
2022-12-03 23:26:56 -0600

    add day 4 solution

---
+++ src/main/kotlin/aoc2022/day_4.kt
@@ -0,0 +1,26 @@
+package aoc2022
+
+fun main() {
+    val input = readLines("input/day4.txt")
+
+    var fullyOverlap = 0
+    var partlyOverlap = 0
+    for (sectionPair in input) {
+        val sectionPairs: List<String> = sectionPair.split(",")
+        val firstSectionBoundaries: List<Int> = sectionPairs[0].split("-").map{ it.toInt() }
+        val sectionSectionBoundaries: List<Int> = sectionPairs[1].split("-").map{ it.toInt() }
+
+        val firstSectionRange = IntRange(firstSectionBoundaries[0], firstSectionBoundaries[1])
+        val secondSectionRange = IntRange(sectionSectionBoundaries[0], sectionSectionBoundaries[1])
+
+        if (firstSectionRange.intersect(secondSectionRange).size in setOf(firstSectionRange.count(), secondSectionRange.count())) {
+            fullyOverlap++
+        }
+
+        if (firstSectionRange.intersect(secondSectionRange).isNotEmpty()) {
+            partlyOverlap++
+        }
+    }
+    println(fullyOverlap)
+    println(partlyOverlap)
+}
